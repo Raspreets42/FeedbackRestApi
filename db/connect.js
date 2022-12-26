09400ec9409b74a133e6577133df8e6f94393efd
Raspreet singh <raspreets89@gmail.com>
2022-12-26 21:51:52 +0530

    code for changing database uri

--- db/connect.js
+++ db/connect.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const uri = "mongodb+srv://raspreetdb:dbpreetsingh@cluster0.lh13d.mongodb.net/raspreetportfolio?retryWrites=true&w=majority";
+
 const connectDB = (uri) => {
     return mongoose.connect(uri, {
         useNewUrlParser: true,
